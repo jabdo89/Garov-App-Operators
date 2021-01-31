f2577c569b57c7090705c679ba329d9c3ac8f02d
ernestognw <ernestognw@gmail.com>
2021-01-31 13:14:26 -0600

    feature(linter): Add linting configuration

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+export default App;
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
-module.exports = function(api) {
+module.exports = (api) => {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@templates': './templates',
+            '@config': './config',
+            '@assets': './assets',
+            '@screens': './screens',
+            '@providers': './providers',
+          },
+        },
+      ],
+    ],
   };
 };
